76b4651e84f021dafd6237e76a3ae6cecfc1390d
Lev-Kostylev <130217523+Lev-Kostylev@users.noreply.github.com>
2023-04-26 20:57:32 +0500

    + все файлы
    Загрузка существующих файлов с компа

---
+++ main.py
@@ -0,0 +1,124 @@
+import sys
+from time import time as timer
+from pygame import *
+from random import random, randint
+font.init()
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed_x, player_speed_y):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (100, 100))
+        self.speed_x = player_speed_x
+        self.speed_y = player_speed_y
+        self.speed = player_speed_x
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+    
+class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed_x, player_speed_y):
+        super().__init__(player_image, player_x, player_y, player_speed_x, player_speed_y)
+        self.flag_for_score = True
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_a] or keys_pressed[K_LEFT]:
+            self.rect.x -= self.speed
+        if keys_pressed[K_d] or keys_pressed[K_RIGHT]:
+            self.rect.x += self.speed
+        if keys_pressed[K_w] or keys_pressed[K_UP]:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] or keys_pressed[K_DOWN]:
+            self.rect.y += self.speed
+    def update_score(self):
+        global score
+        global last_time
+        global new_time
+        global flag_for_score
+        if flag_for_score == True:
+            last_time = timer()
+            flag_for_score = False
+        new_time = timer()
+        if (new_time - last_time) >= 1:
+            score += 1
+
+class Enemy(GameSprite):
+    def update(self, enemy_type):
+        if enemy_type == 'killer':
+            if player.rect.x >= self.rect.x:
+                self.rect.x += self.speed
+            else:
+                self.rect.x -= self.speed
+            if player.rect.y >= self.rect.y:
+                self.rect.y += self.speed
+            else:
+                self.rect.y -= self.speed
+        elif enemy_type == 'enemy':
+            if self.speed_x > 0:
+                self.rect.x += self.speed_x
+            elif self.speed_x < 0:
+                self.rect.x -= self.speed_x
+            if self.speed_y > 0:
+                self.rect.y += self.speed_y
+            elif self.speed_y < 0:
+                self.rect.y -= self.speed_y
+        elif enemy_type == 'line_enemy':
+            ...
+
+#setup
+with open('score.txt', 'r', encoding='utf-8') as file:
+    score = int(file.read())
+flag_for_score = True #данный флаг показывает, надо ли обнулять ласт тайм
+last_time = timer()
+new_time = timer()
+x = [2, 2, 2, -3, -4, -3]
+y = [1, 2, 4, -4, -1, -3]
+
+window = display.set_mode((1920, 1020))
+display.set_caption('GoFaster!')
+display.set_icon(transform.scale(image.load('player.png'), (1920, 1080)))
+clock = time.Clock()
+background = transform.scale(image.load('background.png'), (1920, 1080))
+
+player = Player('player.png', 910, 490, 10, None)
+killer = Enemy('killer.png', 910, 490, 2, None)
+
+enemies = sprite.Group()
+for i in range(1, 6):
+    enemy = Enemy('enemy.png', randint(0, 635), 0, x[i], y[i])
+    enemies.add(enemy)
+line_enemies = sprite.Group()
+for i in range(1, 6):
+    line_enemy = Enemy('line_enemy.png', randint(0, 635), 0, x[i], y[i])
+    line_enemies.add(line_enemy)
+
+font2 = font.Font(None, 36)
+
+while True:
+    for e in event.get():
+        if e.type == QUIT:
+            sys.exit()
+
+    window.blit(background, (0, 0))
+    text_score = font2.render('Счёт: ' + str(score), 1, (255, 255, 255))
+    window.blit(text_score, (100, 300))
+
+    killer.update('killer')
+    killer.reset()
+
+    enemies.update('enemy9999999999')
+    enemies.draw(window)
+    line_enemies.update('killer')
+    line_enemies.draw(window)
+
+    player.update()
+    player.reset()
+    player.update_score()
+    
+    display.update()
+    clock.tick(60)
+
+    with open('score.txt', 'w', encoding='utf-8') as file:
+        file.write(str(score))
